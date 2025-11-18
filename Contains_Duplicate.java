16a217db80febc5107c23415cc4c5ff7d5513e4a
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2025-11-18 10:51:47 -0600

    Update and rename Solution1.java to Contains_Duplicate.java

--- Contains_Duplicate.java
+++ Contains_Duplicate.java
@@ -1,6 +1,6 @@
 package org.example;
 
-class Solution1 {
+class Contains_Duplicate {
     public boolean hasDuplicate(int[] nums) {
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
